311b04e5dfcdd7ef4f8424438ca0d50ff30bc866
songsong <dragonh0919@gmail.com>
2023-01-04 19:18:41 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to my web site!</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h2>About me</h2>
+        <p>I'm a web developer and designer. I love building beautiful and functional websites.</p>
+      </main>
+      <footer>
+        <p>Copyright 2021</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
